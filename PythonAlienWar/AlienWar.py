fe4a2a23cb07e5789e7c00ff8a76f744a24ea1b6
XieTian <Stevens@Stevens-MacBook-Air.local>
2017-12-14 11:56:57 +0800

    ⚒：sys_changes

--- PythonAlienWar/AlienWar.py
+++ PythonAlienWar/AlienWar.py
@@ -1,8 +1,9 @@
-import sys
+
 import pygame
 
 from settings import settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -13,9 +14,7 @@ def run_game():
     ship = Ship(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         screen.fill(ai_settings.bg_color)
 
